5c4d1eb8bdaa97d6f0d465c233c88464c6b6986d
DonatoRM <donatoramos@hotmail.com>
2023-02-21 01:07:55 +0100

    Finalizada la lógica de la página preferencias

--- src/preferencias.php
+++ src/preferencias.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <?php
+// Abrimos una sesión
 session_start();
 ?>
 <html lang="es">
@@ -16,6 +17,17 @@ session_start();
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="../css/estilos.css">
 </head>
+<?php
+if (isset($_POST['establecer'])) {
+    $_SESSION['idioma'] = $_POST['idioma'];
+    $_SESSION['perfil'] = $_POST['perfil'];
+    $_SESSION['zonaHoraria'] = $_POST['zonaHoraria'];
+    $archivoActual = $_SERVER['PHP_SELF'];
+    $_SESSION['mensaje'] = 'Preferencias de usuario guardadas';
+    header("Location: $archivoActual");
+    exit();
+}
+?>
 
 <body class="container bg-fondo-claro">
     <main>
@@ -28,6 +40,16 @@ session_start();
                         </h4>
                     </div>
                 </div>
+                <?php
+                if (isset($_SESSION['mensaje'])) {
+                    echo '<div class="row mt-1 mb-1">';
+                    echo '<div class="col">';
+                    echo "<p class='text-azul_oscuro'>" . $_SESSION['mensaje'] . "</p>";
+                    echo '</div>';
+                    echo '</div>';
+                    unset($_SESSION['mensaje']);
+                }
+                ?>
                 <div class="row mt-1 mb-1">
                     <div class="col">
                         <label for="idIdioma">Idioma</label>
@@ -38,8 +60,23 @@ session_start();
                         <i class="fa-solid fa-language fa-md pt-2"></i>
                     </div>
                     <select class="col form-select me-3" name="idioma" id="idIdioma">
-                        <option value='es' selected>Español</option>
-                        <option value='en'>Inglés</option>
+                        <?php
+                        if (isset($_SESSION['idioma'])) {
+                            if ($_SESSION['idioma'] == 'es') {
+                                echo "<option value='es' selected>Español</option>";
+                            } else {
+                                echo "<option value='es'>Español</option>";
+                            }
+                            if ($_SESSION['idioma'] == 'en') {
+                                echo "<option value='en' selected>Inglés</option>";
+                            } else {
+                                echo "<option value='en'>Inglés</option>";
+                            }
+                        } else {
+                            echo "<option value='es'>Español</option>";
+                            echo "<option value='en'>Inglés</option>";
+                        }
+                        ?>
                     </select>
                 </div>
                 <div class="row mt-1 mb-1">
@@ -52,8 +89,23 @@ session_start();
                         <i class="fa-solid fa-users fa-md pt-2"></i>
                     </div>
                     <select class="col form-select me-3" name="perfil" id="idPerfil">
-                        <option value='yes' selected>Sí</option>
-                        <option value='no'>No</option>
+                        <?php
+                        if (isset($_SESSION['perfil'])) {
+                            if ($_SESSION['perfil'] == 'yes') {
+                                echo "<option value='yes' selected>Sí</option>";
+                            } else {
+                                echo "<option value='yes'>Sí</option>";
+                            }
+                            if ($_SESSION['perfil'] == 'en') {
+                                echo "<option value='no' selected>No</option>";
+                            } else {
+                                echo "<option value='no'>No</option>";
+                            }
+                        } else {
+                            echo "<option value='yes'>Sí</option>";
+                            echo "<option value='no'>No</option>";
+                        }
+                        ?>
                     </select>
                 </div>
                 <div class="row mt-1 mb-1">
@@ -66,17 +118,60 @@ session_start();
                         <i class="fa-regular fa-clock fa-md pt-2"></i>
                     </div>
                     <select class="col form-select me-3" name="zonaHoraria" id="idZonaHoraria">
-                        <option value='-2' selected>GMT-2</option>
-                        <option value='-1'>GMT-1</option>
-                        <option value='0'>GMT</option>
-                        <option value='1'>GMT+1</option>
-                        <option value='2'>GMT+2</option>
+                        <?php
+                        if (isset($_SESSION['zonaHoraria'])) {
+                            switch ($_SESSION['zonaHoraria']) {
+                                case '-2':
+                                    echo "<option value='-2' selected>GMT-2</option>";
+                                    echo "<option value='-1'>GMT-1</option>";
+                                    echo "<option value='0'>GMT</option>";
+                                    echo "<option value='1'>GMT+1</option>";
+                                    echo "<option value='2'>GMT+2</option>";
+                                    break;
+                                case '-1':
+                                    echo "<option value='-2'>GMT-2</option>";
+                                    echo "<option value='-1' selected>GMT-1</option>";
+                                    echo "<option value='0'>GMT</option>";
+                                    echo "<option value='1'>GMT+1</option>";
+                                    echo "<option value='2'>GMT+2</option>";
+                                    break;
+                                case '0':
+                                    echo "<option value='-2'>GMT-2</option>";
+                                    echo "<option value='-1'>GMT-1</option>";
+                                    echo "<option value='0' selected>GMT</option>";
+                                    echo "<option value='1'>GMT+1</option>";
+                                    echo "<option value='2'>GMT+2</option>";
+                                    break;
+                                case '1':
+                                    echo "<option value='-2'>GMT-2</option>";
+                                    echo "<option value='-1'>GMT-1</option>";
+                                    echo "<option value='0'>GMT</option>";
+                                    echo "<option value='1' selected>GMT+1</option>";
+                                    echo "<option value='2'>GMT+2</option>";
+                                    break;
+                                case '2':
+                                    echo "<option value='-2'>GMT-2</option>";
+                                    echo "<option value='-1'>GMT-1</option>";
+                                    echo "<option value='0'>GMT</option>";
+                                    echo "<option value='1'>GMT+1</option>";
+                                    echo "<option value='2' selected>GMT+2</option>";
+                                    break;
+                            }
+                        } else {
+                            echo "<option value='-2'>GMT-2</option>";
+                            echo "<option value='-1'>GMT-1</option>";
+                            echo "<option value='0'>GMT</option>";
+                            echo "<option value='1'>GMT+1</option>";
+                            echo "<option value='2'>GMT+2</option>";
+                        }
+                        ?>
+
                     </select>
                 </div>
                 </div>
                 <div class="row pb-4">
                     <div class="col">
-                        <a href="#" class="btn btn-azul_oscuro">Mostrar Preferencias</a>
+                        <a href="mostrar.php" class="btn btn-azul_oscuro">Mostrar Preferencias</a>
                     </div>
                     <div class="col">
                         <button type="submit" name="establecer" value='ok' class="btn btn-verde_lima text-white">Establecer Preferencias</button>
